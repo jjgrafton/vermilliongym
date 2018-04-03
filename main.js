933e672f79d71d9e9f52440a70f26d70e1cda7bf
maiya-22 <maiya_o@yahoo.com>
2018-04-02 23:08:46 -0400

    api promises being fetched, via promises, and Player.prototype.loadGym() function working

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 /* eslint func-names: 0,  no-unused-vars: 0, no-alert: 0, class-methods-use-this: 0 , no-plusplus: 0 , indend: 0 , no-restricted-syntax: 0 , no-use-before-define: 0 , no-loop-func: 0, func-names: 0, space-before-blocks: 0, indent: 0, max-len: 0 */
+// The comments above are just to remove error messages from my VS Code
 
 window.onload = function (evt) {
-  console.log('js loaded');
-  // TRAINER CLASS
-
+  // CHARACTER CLASS, USED FOR OUR POKEMON
   class Character {
     constructor(characterName, pic, gif, stats, abilities) {
       this.name = characterName;
@@ -12,27 +11,38 @@ window.onload = function (evt) {
       this.stats = stats;
       this.abilities = abilities;
     }
+    // function goes to the api and gets a pokemon object, by its name
+    // returns a promise for the object, instead of the actual object
     getCharacterPromise(characterName) {
       const endpoint = characterName || this.name;
+      // this function will return a promise:
       return new Promise((resolve, reject) => {
+        // create a new request object:
         const xhr = new XMLHttpRequest();
+        // format the request object, to get it ready to send:
         xhr.open('GET', `${this.apiURL}/${endpoint}/`);
+        // send that thing:
         xhr.send();
+        // this is an event-listener.  When the 'onload'event fires, it means that there
+        // has been a successful response:
         xhr.onload = function () {
+          // notice that we are wrapping the data in the 'resolve' function, and this is the name of the parameter on line 19
           resolve(JSON.parse(xhr.responseText));
         };
+        // this is an event-listener.  When the 'onerror' event fires, it means that there
+        // the response is an error
         xhr.onerror = function () {
+          // notice that we are wrapping the data in the 'reject' function, and this is the name of the parameter on line 19
           reject(xhr.statusText);
         };
       });
     }
-    makeCharacterInstance(characterObject, gif = 'no gif') {
-      // take all of the data on the object and format it in a clean, simple way, in order to pass
-      // what we need to our contructor function:
-      console.log(characterObject);
-      // get the character name:
+    makeCharacterInstance(characterObject) {
+      // take all of the data in the object that the a.p.i. returns
+      // and extract it in a way that allows us to neatly pass it to
+      // our constructor function:
       const characterName = characterObject.name;
-      // re-format the stats into an object, using the reduce method:
+      // This reduce thing can make you dizzy when you first look at it.  Will go over it with you.
       const reformattedStats = characterObject.stats.reduce((reformatted, stat) => {
         reformatted[stat.stat.name] = stat.base_stat;
         return reformatted;
@@ -41,88 +51,57 @@ window.onload = function (evt) {
         arr[index] = ability.ability.name;
         return arr;
       }, []);
-      console.log('reformattedAbilities: ', reformattedAbilities);
       const defaultPic = characterObject.sprites.front_default;
-      console.log('reformatted stats: ', reformattedStats);
-      console.log('characterName: ', characterName);
-      const character = new Character(
-        characterName,
-        defaultPic,
-        gif,
-        reformattedStats,
-        reformattedAbilities,
-      );
-      console.log('Character Instance: ', character);
+      // if our hash of gifs, contains a gif for this character, add it:
+      // if not, store it as a 'null' value
+      const gif = Character.prototype.gifs[characterName] || null;
+      // create an instance of a Character constructor function/ class, and return it.
+      return new Character(characterName, defaultPic, gif, reformattedStats, reformattedAbilities);
     }
   }
   Character.prototype.apiURL = 'https://pokeapi.co/api/v2/pokemon';
-  //   Pokemon names: Oddish, Gloom and Weezing
-
-  // Chuck's Pokemon:
-  // const dragonair = new Character('dragonair');
-  // const getDragonair = dragonair.getCharacterPromise();
-  // getDragonair.then((character) => {
-  //   console.log(character);
-  // });
-  // const butterfree = new Character('butterfree');
-  // const getButterfree = butterfree.getCharacterPromise();
-  // getButterfree.then((character) => {
-  //   console.log(character);
-  // });
-  // const charmeleon = new Character('charmeleon');
-  // const getCharmeleon = charmeleon.getCharacterPromise();
-  // getCharmeleon.then((character) => {
-  //   console.log(character);
-  // });
-
-  // Professor Gloom's Pokemon:
-
-  getOddish = Character.prototype.getCharacterPromise('oddish');
-  getOddish.then((character) => {
-    // const oddish = Character.prototype.makeCharacterInstance(character);
-    const gif = 'add gif URL here';
-    Character.prototype.makeCharacterInstance(character, gif);
-  });
-  // const weezing = new Character('weezing');
-  // getWeezing = weezing.getCharacterPromise();
-  // getWeezing.then((character) => {
-  //   console.log(character);
-  // });
-
-  // getGloom = Character.prototype.getCharacterPromise();
-  // getGloom.then((character) => {
-  //   const gloom = new Character();
-  //   console.log('Gloom: ', gloom);
-  // });
+  // since we are adding external gifs, we will store whichever gifs we are able to find here:
+  Character.prototype.gifs = {
+    dragonair: 'add gif',
+    charmeleon: 'add gif',
+    butterfree: 'add gif',
+    weezing: 'add gif',
+    oddish: 'add gif',
+    gloom: 'add gif',
+  };
 
   class Player {
-    constructor(playerName) {
+    constructor(playerName, charactersArray) {
       this.name = playerName;
       this.gym = {};
+      this.characters = charactersArray;
     }
     loadGym(arrayOfCharacters) {
+      const charactersArray = this.characters || arrayOfCharacters;
       const arrayOfPromises = [];
-      for (let i = 0; i < arrayOfCharacters.length; i++) {
-        const characterName = arrayOfCharacters[i];
+      for (let i = 0; i < charactersArray.length; i++) {
+        const characterName = charactersArray[i];
         const promise = Character.prototype.getCharacterPromise(characterName);
         arrayOfPromises[i] = promise;
       }
+      Promise.all(arrayOfPromises)
+        .then((arrayOfCharacterObjects) => {
+          arrayOfCharacterObjects.forEach((characterObject) => {
+            this.gym[characterObject.name] = characterObject;
+          });
+          console.log(`${this.name}'s gym: `, this.gym);
+        })
+        .catch((err) => {
+          console.log(`error caught in the loadGym promise chain: ${err}`);
+        });
     }
   }
 
-  // how to do async "when" the charactersLoaded === 6 ?
-  const display = {
-    charactersLoaded: 0,
-  };
+  // here we create our player instances, and pass an array of our pokemon names:
+  const professorDoom = new Player('Professor Doom', ['weezing', 'oddish', 'gloom']);
+  const chuck = new Player('Chuck', ['dragonair', 'butterfree', 'charmeleon']);
 
-  const professorDoom = new Player('Professor Doom');
-  const chuck = new Player('Chuck');
-
-  //   const dragonairPromise = Character.prototype.getCharacterPromise('dragonair');
-  //   dragonairPromise.then((character) => {
-  //     console.log(character);
-  //   })
-  //   .catch(function(err) => {
-  //       console.log(err);
-  //   });
+  // here, we call the .loadGym function on each of the players:
+  chuck.loadGym();
+  professorDoom.loadGym();
 };
